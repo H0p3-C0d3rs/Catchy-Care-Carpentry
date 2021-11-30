92783485c0e80deddbddf133b65ef2b241abcfe0
mohammadkabbara <mohammadkabbara40@gmail.com>
2021-11-30 21:39:32 +0200

    finish profile design

---
+++ app/src/main/java/us/hopecoders/catchy_care_carpentry/splash/Splash.java
@@ -0,0 +1,28 @@
+package us.hopecoders.catchy_care_carpentry.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import us.hopecoders.catchy_care_carpentry.R;
+import us.hopecoders.catchy_care_carpentry.intros.OnBoardingActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            //This method will be executed once the timer is over
+            // Start your app main activity
+            startActivity(new Intent(Splash.this, OnBoardingActivity.class));
+            // close this activity
+            finish();
+        }, 1000); // pause the launch of the dashboard for 3 seconds
+    }
+}
